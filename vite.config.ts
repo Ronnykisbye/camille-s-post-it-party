332180721484a7e5a0e187e8997ea1d77f3122c6
Ronny kisbye <111735396+Ronnykisbye@users.noreply.github.com>
2026-01-13 13:08:09 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/camille-30aar/',   // 👈 MEGET VIGTIG
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true
-  }
+  base: '/camille-s-post-it-party/',
 })
+
